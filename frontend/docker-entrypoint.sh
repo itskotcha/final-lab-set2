2abea11c2dafe4c4c7e0562e3d6fc0c75b83108d
Warissara67 <eropalne@gmail.com>
2026-03-17 04:55:42 +0700

    fix: use $PORT for Railway deployment

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -4,4 +4,8 @@ window.ENV_AUTH_URL = "${AUTH_SERVICE_URL:-}";
 window.ENV_TASK_URL = "${TASK_SERVICE_URL:-}";
 window.ENV_USER_URL = "${USER_SERVICE_URL:-}";
 ENVEOF
+
+# Substitute $PORT ใน nginx config
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
 exec "$@"
